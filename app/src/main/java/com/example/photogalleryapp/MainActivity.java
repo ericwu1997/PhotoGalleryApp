86679d27c887fa38b0155793f27c5f16e9b3f518
Xinghuawei <wxh52794@gmail.com>
2019-10-02 19:58:41 -0700

    Task 6
    Task 6: Search image files and display image from the file.
    Enable left image button and right image button to swin left and right

--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.photogalleryapp.Manager.CameraManager;
 import com.example.photogalleryapp.Manager.PhotoDisplayManager;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.sql.Timestamp;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private PhotoDisplayManager photoDisplayManager;
     private CameraManager cameraManager;
+    private ArrayList<String> photoGallery;
+    private String currentPhotoPath=null;
+    private int currentPhotoIndex = 0;
 
     //Testing
     ImageView image_photoDisplay;
@@ -60,7 +74,60 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // PhotoDisplay imageView
-        image_photoDisplay = findViewById(R.id.image_photoDisplay);
+        //image_photoDisplay = findViewById(R.id.image_photoDisplay);
+        ImageButton btnLeft = findViewById(R.id.button_left);
+        ImageButton btnRight = findViewById(R.id.button_right);
+        btnLeft.setOnClickListener(this);
+        btnRight.setOnClickListener(this);
+        Date minDate = new Date(Long.MIN_VALUE);
+        Date maxDate = new Date(Long.MAX_VALUE);
+        photoGallery = populateGallery(minDate, maxDate);
+        Log.d("onCreate, size", Integer.toString(photoGallery.size()));
+        if (photoGallery.size() > 0)
+            currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        displayPhoto(currentPhotoPath);
+    }
+
+
+
+    private ArrayList<String> populateGallery(Date minDate, Date maxDate) {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/Android/data/com.example.photogalleryapp/files/Pictures");
+        ArrayList<String> photoGallery = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : file.listFiles()) {
+                photoGallery.add(f.getPath());
+            }
+        }
+        return photoGallery;
+    }
+
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.image_photoDisplay);
+        iv.setImageBitmap(BitmapFactory.decodeFile(path));
+    }
+
+    public void onClick( View v) {
+        switch (v.getId()) {
+            case R.id.button_left:
+                --currentPhotoIndex;
+                break;
+            case R.id.button_right:
+                ++currentPhotoIndex;
+                break;
+            default:
+                break;
+        }
+        if (currentPhotoIndex < 0)
+            currentPhotoIndex = 0;
+        if (currentPhotoIndex >= photoGallery.size())
+            currentPhotoIndex = photoGallery.size() - 1;
+
+        currentPhotoPath = photoGallery.get(currentPhotoIndex);
+        Log.d("phpotoleft, size", Integer.toString(photoGallery.size()));
+        Log.d("photoleft, index", Integer.toString(currentPhotoIndex));
+        displayPhoto(currentPhotoPath);
     }
 
     @Override
